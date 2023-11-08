667e6cb8b6d3628ff8aebb86e2294f0dd33a58e4
Siva22Raman <148310496+Siva22Raman@users.noreply.github.com>
2023-11-09 00:20:15 +0530

    Update nbar.jsx

--- src/component/nbar.jsx
+++ src/component/nbar.jsx
@@ -1,29 +1,31 @@
 import React, { useState } from 'react';
 import { RiShoppingCart2Fill } from 'react-icons/ri';
 import { AiOutlineUser } from 'react-icons/ai';
-import {BsInfoCircleFill} from 'react-icons/bs'
+import { BsInfoCircleFill } from 'react-icons/bs';
 import { BiSolidLogIn, BiSolidLogOut } from 'react-icons/bi';
 import Dropdown from 'react-bootstrap/Dropdown';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../redux/cartSlice';
-import {clearFromcart} from '../redux/cartSlice';
+import { clearFromcart } from '../redux/cartSlice';
 import { MdSell } from 'react-icons/md';
 import { toast } from 'react-toastify';
+import { Link } from 'react-router-dom'; // Import Link component
 import './Navbar.css';
 
-function Nabar() {
-  const islog = useSelector((state) => state.cart.isAuthenticated);
-  const us = useSelector((state) => state.cart.user);
+function Navbar() {
+  const isLog = useSelector((state) => state.cart.isAuthenticated);
+  const user = useSelector((state) => state.cart.user);
   const dispatch = useDispatch();
 
-  const ha = () => {
+  const handleLogout = () => {
     dispatch(clearFromcart());
     dispatch(logout());
   }
-  const h = () => {
-    if(islog){
-    }
-    else{
+
+  const handleSell = () => {
+    if (isLog) {
+      // Handle sell logic here
+    } else {
       toast.error("Login to Sell products");
     }
   }
@@ -34,10 +36,10 @@ function Nabar() {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/" style={{ marginLeft: '30px' }}>
-      <img src="https://img.freepik.com/free-vector/fruit-label-sticker_17005-1006.jpg?size=626&ext=jpg&ga=GA1.1.632798143.1698796800&semt=ais" alt="Shanu Fruits Logo" style={{ width: '40px' }} /> 
+      <Link className="navbar-brand" to="/" style={{ marginLeft: '30px' }}>
+        <img src="https://img.freepik.com/free-vector/fruit-label-sticker_17005-1006.jpg?size=626&ext=jpg&ga=GA1.1.632798143.1698796800&semt=ais" alt="Shanu Fruits Logo" style={{ width: '40px' }} />
         Shanu Fruits
-      </a>
+      </Link>
       <button
         className="navbar-toggler"
         type="button"
@@ -56,59 +58,70 @@ function Nabar() {
         }`}
         style={{ marginRight: '30px', backgroundColor: 'none' }}
         id="navbarNav"
-        >
+      >
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <a className="nav-link" href="/">
+            <Link className="nav-link" to="/">
               Home
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/l">
+            <Link className="nav-link" to="/l">
               About
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/con">
+            <Link className="nav-link" to="/con">
               Contact
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
-            {/* Dropdown for Buy/Sell */}
             <Dropdown>
-              <Dropdown.Toggle variant="light" id="buy-sell-dropdown" onMouseEnter={()=>{h()}}>
-                 Sell
+              <Dropdown.Toggle variant="light" id="buy-sell-dropdown" onMouseEnter={handleSell}>
+                Sell
               </Dropdown.Toggle>
               <Dropdown.Menu>
-                {islog ? (<div>
-                <Dropdown.Item href="/oo">Sell</Dropdown.Item>
-                <Dropdown.Item href="/">Buy</Dropdown.Item>
-                </div>):
-                (<div>
-                   <Dropdown.Item href="/log">Login</Dropdown.Item>
-                </div>)}
-                
+                {isLog ? (
+                  <div>
+                    <Link to="/oo" className="dropdown-item">
+                      Sell
+                    </Link>
+                    <Link to="/" className="dropdown-item">
+                      Buy
+                    </Link>
+                  </div>
+                ) : (
+                  <div>
+                    <Dropdown.Item>Login</Dropdown.Item>
+                  </div>
+                )}
               </Dropdown.Menu>
             </Dropdown>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/o">
+            <Link className="nav-link" to="/o">
               <RiShoppingCart2Fill />
-            </a>
+            </Link>
           </li>
-          {islog ? (
+          {isLog ? (
             <li className="nav-item">
               <Dropdown>
                 <Dropdown.Toggle variant="light" id="user-dropdown">
-                  <AiOutlineUser></AiOutlineUser>&nbsp;{us.uname}
+                  <AiOutlineUser></AiOutlineUser>&nbsp;{user.uname}
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
-                  <Dropdown.Item onClick={() => { ha() }}>
+                  <Dropdown.Item onClick={handleLogout}>
                     <BiSolidLogOut /> &nbsp;Logout
                   </Dropdown.Item>
-                  <Dropdown.Item  onClick={()=>toast.success(`Name:${us.uname} email:${us.umail} Phone:${us.phone} Uid:${us.uid}`)}><BsInfoCircleFill></BsInfoCircleFill>&nbsp;Details</Dropdown.Item>
-                  <Dropdown.Item href="/ps" > <MdSell></MdSell> &nbsp;To Sell</Dropdown.Item>
-                  <Dropdown.Item href="/order" > <MdSell></MdSell> &nbsp;Your order</Dropdown.Item>
+                  <Dropdown.Item onClick={() => toast.success(`Name: ${user.uname} Email: ${user.umail} Phone: ${user.phone} Uid: ${user.uid}`)}>
+                    <BsInfoCircleFill></BsInfoCircleFill>&nbsp;Details
+                  </Dropdown.Item>
+                  <Link to="/ps" className="dropdown-item">
+                    <MdSell></MdSell> &nbsp;To Sell
+                  </Link>
+                  <Link to="/order" className="dropdown-item">
+                    <MdSell></MdSell> &nbsp;Your order
+                  </Link>
                 </Dropdown.Menu>
               </Dropdown>
             </li>
@@ -119,4 +132,4 @@ function Nabar() {
   );
 }
 
-export default Nabar;
+export default Navbar;
