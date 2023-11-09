59b55760b848a9d38a25a68904328a0e817ed8dc
Siva22Raman <148310496+Siva22Raman@users.noreply.github.com>
2023-11-09 09:04:18 +0530

    Update nbar.jsx

--- src/component/nbar.jsx
+++ src/component/nbar.jsx
@@ -66,7 +66,7 @@ function Navbar() {
             </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/l">
+            <Link className="nav-link" to="/log">
               About
             </Link>
           </li>
